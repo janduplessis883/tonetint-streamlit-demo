e7b67930e87e5a1363bfac518ab5de81d4d1f197
Jan du Plessis <drjanduplessis@icloud.com>
2024-09-20 11:28:39 +0100

    fix requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if button:
         visualizer = init_model()  # Initialize the ToneTint model
         html_content = visualizer.visualize(text_area)  # Generate HTML from analyzed text
 
-    with st.container("Output"):
+    with st.container(border=True):
         st.markdown(html_content, unsafe_allow_html=True)  # Display the HTML content
 
     # Convert HTML content to bytes
